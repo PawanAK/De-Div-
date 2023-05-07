8af993ab12ff585f58b403691d45849f1ded135d
PawanAjjarK <1nc20ci027@ncetmail.com>
2023-05-07 08:46:35 +0530

    lol changes

--- src/hero.jsx
+++ src/hero.jsx
@@ -27,14 +27,20 @@ export default function Hero({ transactions }) {
   }, [transactions]);
 
   return (
-    <div className="hero">
-      <TableContainer component={Paper}>
+    <div className="hero bg-purple-100 p-4 transition-all duration-300 ease-in-out">
+      <TableContainer
+        component={Paper}
+        className="bg-green-100 rounded-lg shadow-md">
         <Table className="hero__table" aria-label="simple table">
           <TableHead>
             {showTableHeaders && (
-              <TableRow>
-                <TableCell>To</TableCell>
-                <TableCell>Value</TableCell>
+              <TableRow className="bg-purple-200">
+                <TableCell className="p-2 font-semibold text-purple-600">
+                  To
+                </TableCell>
+                <TableCell className="p-2 font-semibold text-purple-600">
+                  Value
+                </TableCell>
               </TableRow>
             )}
           </TableHead>
@@ -43,9 +49,11 @@ export default function Hero({ transactions }) {
               .filter((tx) => tx.value > 0)
               .sort((a, b) => a.timestamp - b.timestamp)
               .map((tx) => (
-                <TableRow className="transactions" key={tx.hash}>
-                  <TableCell>{tx.to}</TableCell>
-                  <TableCell>
+                <TableRow
+                  className="transactions bg-green-200 hover:bg-green-300 transition-all duration-200 ease-in-out"
+                  key={tx.hash}>
+                  <TableCell className="p-2">{tx.to}</TableCell>
+                  <TableCell className="p-2">
                     {(tx.value / 1000000000000000000).toFixed(8)}
                   </TableCell>
                   <TableCell>
